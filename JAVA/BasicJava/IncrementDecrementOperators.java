1e93e8f7e5721ceedb60d3245f37a1929e63fef7
Janvi Singh <janvishishodiya4@gmail.com>
2026-08-18 14:06:19 +0530

    Added Increment and Decrement Operators practice with edge cases

---
+++ JAVA/BasicJava/IncrementDecrementOperators.java
@@ -0,0 +1,119 @@
+public class IncrementDecrementOperators {
+    public static void main(String[] args) {
+        /*
+         * byte b=127;
+         * b++;
+         * System.out.println(b);
+         */
+
+
+        /*
+         * int a=78;
+         * int b=a++;
+         * int c=--b;
+         * c++;
+         * b--;
+         * System.out.println(a);
+         * System.out.println(b);
+         * System.out.println(c);
+         */
+
+
+        /*
+         * int a=10;
+         * System.out.println(++a);
+         * System.out.println(a);
+         */
+
+
+        /*
+         * int a=10;
+         * System.out.println(a++);
+         * System.out.println(a);
+         */
+
+
+        /*
+         * int a=10;
+         * int b=a++;
+         * int c=++a;
+         * System.out.println(a);
+         * System.out.println(b);
+         * System.out.println(c);
+         */
+
+
+        /*
+         * int a=19;
+         * int b=++a;
+         * int c=++b;
+         * System.out.println(a);
+         * System.out.println(b);
+         * System.out.println(c);
+         */
+
+
+        /*
+         * int k=17;
+         * int j=k++;
+         * int n=k++;
+         * System.out.println(k);
+         * System.out.println(j);
+         * System.out.println(n);
+         */
+
+
+        /*
+         * int a=10;
+         * a=a++;
+         * a=a++;
+         * System.out.println(a);
+         */
+
+
+        /*
+         * int a=10;
+         * System.out.println(--a);
+         * System.out.println(a);
+         */
+
+
+        /*
+         * int a=10;
+         * System.out.println(a--);
+         * System.out.println(a);
+         */
+
+
+        /*
+         * int a=78;
+         * int b=a++;
+         * int c=--b;
+         * c++;
+         * b--;
+         * System.out.println(a);
+         * System.out.println(b);
+         * System.out.println(c);
+         */
+
+
+        /*
+         * int a = 20;
+         * int b = --a;
+         * int c = --a;
+         * a++;
+         * b--;
+         * c++;
+         * System.out.println(a);
+         * System.out.println(b);
+         * System.out.println(c);
+         */
+
+        
+        byte b = 127;
+        b++;
+        System.out.println(b);
+
+    }
+
+}
