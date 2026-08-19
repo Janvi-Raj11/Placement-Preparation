9a5e497be1ee60c7830fa34b16fc3ef31c507204
Janvi Singh <janvishishodiya4@gmail.com>
2026-08-19 13:38:18 +0530

    Fixed second largest distinct element logic - TC O(n), SC O(1)

--- JAVA/CompanyQuestions/SecondLargestElement.java
+++ JAVA/CompanyQuestions/SecondLargestElement.java
@@ -1,7 +1,7 @@
 
 class SecondLargestElement {
     public static void main(String[] args) {
-        int[] arr = { 50, 10, 20, 30, 40 };
+        int[] arr = { 10, 50, 20, 50, 30 };
         int first_lar = arr[0];
         int sec_lar = Integer.MIN_VALUE;
         int res = SecondLargest(arr, first_lar, sec_lar);
@@ -15,7 +15,7 @@ class SecondLargestElement {
             if (first_lar < arr[i]) {
                 sec_lar = first_lar;
                 first_lar = arr[i];
-            } else if (sec_lar < arr[i]) {
+            } else if (sec_lar < arr[i] && arr[i] != first_lar) {
                 sec_lar = arr[i];
             }
 
@@ -26,3 +26,11 @@ class SecondLargestElement {
     }
 
 }
+
+/*Mistake:
+Forgot to check arr[i] != first_lar, which can make the second largest
+equal to the largest when duplicate maximum values are present.
+
+Lesson:
+For second-largest DISTINCT element, always exclude the current largest
+element from becoming second largest.*/
